83891a32dd57f88edc6e540ee9bf58484e8d3a59
Vitalii Zavadskyi <zavadskyi@jinr.ru>
2025-09-24 16:54:54 +0300

    add source-type arg

--- fits/fit_dayabay_dgm.py
+++ fits/fit_dayabay_dgm.py
@@ -124,7 +124,7 @@ if __name__ == "__main__":
         "--source-type",
         "--source",
         choices=("tsv", "hdf5", "root", "npz"),
-        default="hdf5",
+        default="default:hdf5",
         help="data source type",
     )
     model.add_argument(
