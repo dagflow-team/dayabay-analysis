641f96c1011ae432aea44b744df67957969780a0
Vitalii Zavadskyi <zavadskyi@jinr.ru>
2025-09-25 09:48:37 +0300

    update scripts echo

--- scripts/fit_dayabay_iminuit_asimov.sh
+++ scripts/fit_dayabay_iminuit_asimov.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-echo "Simple statistic+all systematic fit with Neyman's chi-squared function"
-echo "Asimov observation"
-echo 'Minimization parameters:'
-echo 'Free: $\Delta m^2_{32}$, $sin^2 2\theta_{13}$ and $\xi_i, i = \overline{0, 18}$'
-echo 'where last parameters are parameters of antineutrino spectra shape'
-echo 'All constrained parameters are used in covariance matrix'
+echo "Simple statistic+all systematic fit with Neyman's chi-squared function\n" \
+     "Asimov observation\n" \
+     "Minimization parameters:\n" \
+     'Free: $\Delta m^2_{32}$, $sin^2 2\theta_{13}$ and $\xi_i, i = \overline{0, 18}$' "\n" \
+     "where last parameters are parameters of antineutrino spectra shape\n" \
+     "All constrained parameters are used in covariance matrix"
 
 ./fits/fit_dayabay_iminuit_asimov.py \
   --chi2 full.covmat.chi2n \
   --free-spectrum-shape
 
 
-echo "Simple statistic+all systematic fit with Pearson's chi-squared function"
-echo "Asimov observation"
-echo 'Minimization parameters:'
-echo 'Free: $\Delta m^2_{32}$, $sin^2 2\theta_{13}$ and $N^{\rm global}$'
-echo "where the last parameter scales observed IBD spectrum in each detector"
-echo "simultaneously"
-echo "All constrained parameters are included as pull terms"
-echo "Uncertainties for spectral parameters are not included"
+echo "Simple statistic+all systematic fit with Pearson's chi-squared function\n" \
+     "Asimov observation\n" \
+     "Minimization parameters:\n" \
+     'Free: $\Delta m^2_{32}$, $sin^2 2\theta_{13}$ and $N^{\rm global}$' "\n" \
+     "where the last parameter scales observed IBD spectrum in each detector\n" \
+     "simultaneously\n" \
+     "All constrained parameters are included as pull terms\n" \
+     "Uncertainties for spectral parameters are not included"
 
 ./fits/fit_dayabay_iminuit_asimov.py \
   --chi2 full.pull.chi2p
