9a520263f897ce414839de321c4c501899bb9628
Vitalii Zavadskyi <zavadskyi@jinr.ru>
2025-10-14 16:27:36 +0300

    feature: add profiling in examples

--- scripts/fit_dayabay_iminuit_asimov.sh
+++ scripts/fit_dayabay_iminuit_asimov.sh
@@ -9,11 +9,13 @@ printf "%s\n" \
      "Minimization parameters:" \
      'Free: $\Delta m^2_{32}$, $sin^2 2\theta_{13}$ and $\xi_i, i = \overline{0, 18}$' \
      "where last parameters are parameters of antineutrino spectra shape" \
-     "All constrained parameters are used in covariance matrix"
+     "All constrained parameters are used in covariance matrix" \
+     '$\Delta m^2_{32}$, $sin^2 2\theta_{13}$ are profiled within Minos procedure'
 
 ./fits/fit_dayabay_iminuit_asimov.py \
     --statistic full.covmat.chi2cnp \
     --free-spectrum-shape \
+    --profile-parameters survival_probability.DeltaMSq32 survival_probability.SinSq2Theta13 \
     --output fit-syst-chi2cnp-free_spectrum_shape-constrained_covmat_all.json
 
 
--- scripts/fit_dayabay_iminuit_monte_carlo.sh
+++ scripts/fit_dayabay_iminuit_monte_carlo.sh
@@ -9,13 +9,15 @@ printf "%s\n" \
      "Minimization parameters:" \
      'Free: $\Delta m^2_{32}$, $sin^2 2\theta_{13}$ and $\xi_i, i = \overline{0, 18}$' \
      "where last parametrs are parameters of antineutrino spectra shape" \
-     "All constrained parameters are used in covariance matrix"
+     "All constrained parameters are used in covariance matrix" \
+     '$\Delta m^2_{32}$, $sin^2 2\theta_{13}$ are profiled within Minos procedure'
 
 ./fits/fit_dayabay_iminuit_monte_carlo.py \
     --statistic full.covmat.chi2cnp \
     --monte-carlo poisson \
     --seed 1 \
     --free-spectrum-shape \
+    --profile-parameters survival_probability.DeltaMSq32 survival_probability.SinSq2Theta13 \
     --output fit-syst-chi2cnp-free_spectrum_shape-constrained_covmat_all.json
 
 
