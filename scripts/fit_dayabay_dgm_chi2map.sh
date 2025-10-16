00cf982b403b3430dc113206fdd9a5ae257e76ab
Vitalii Zavadskyi <zavadskyi@jinr.ru>
2025-10-16 09:53:12 +0300

    update example of contour creating

--- scripts/fit_dayabay_dgm_chi2map.sh
+++ scripts/fit_dayabay_dgm_chi2map.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 ./fits/fit_dayabay_dgm_chi2map.py \
+    --data real \
     --free-parameters survival_probability neutrino_per_fission_factor \
-    --scan-par survival_probability.SinSq2Theta13 0.072 0.098 25 \
-    --scan-par survival_probability.DeltaMSq32 2.17e-3 2.77e-3 25 \
+    --scan-par survival_probability.SinSq2Theta13 0.072 0.098 21 \
+    --scan-par survival_probability.DeltaMSq32 2.17e-3 2.77e-3 21 \
     --chi2 full.covmat.chi2cnp \
-    --output contour.npz
+    --output contour-real-covmat-chi2cnp.npz
+
+./fits/fit_dayabay_dgm_chi2map.py \
+    --data real \
+    --free-parameters survival_probability neutrino_per_fission_factor \
+    --constrained-parameters survival_probability detector reactor background \
+    --scan-par survival_probability.SinSq2Theta13 0.072 0.098 21 \
+    --scan-par survival_probability.DeltaMSq32 2.17e-3 2.77e-3 21 \
+    --chi2 full.covmat.chi2cnp \
+    --output contour-real-pull-chi2cnp.npz
