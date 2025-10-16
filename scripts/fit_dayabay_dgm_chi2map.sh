9016ff01f6ab91ce083faa335f8a8c19150b9c24
Vitalii Zavadskyi <zavadskyi@jinr.ru>
2025-10-16 09:41:33 +0300

    update contour plot script

--- scripts/fit_dayabay_dgm_chi2map.sh
+++ scripts/fit_dayabay_dgm_chi2map.sh
@@ -2,7 +2,7 @@
 
 ./fits/fit_dayabay_dgm_chi2map.py \
     --free-parameters survival_probability neutrino_per_fission_factor \
-    --scan-par survival_probability.SinSq2Theta13 0.072 0.098 15 \
-    --scan-par survival_probability.DeltaMSq32 2.22e-3 2.82e-3 15 \
+    --scan-par survival_probability.SinSq2Theta13 0.072 0.098 25 \
+    --scan-par survival_probability.DeltaMSq32 2.17e-3 2.77e-3 25 \
     --chi2 full.covmat.chi2cnp \
     --output contour.npz
