9d7800f07635c6747104f5603efa07d26245c33e
Vitalii Zavadskyi <zavadskyi@jinr.ru>
2025-10-16 10:54:54 +0300

    update statistic option, add custom scanning

--- fits/fit_dayabay_dgm_chi2map.py
+++ fits/fit_dayabay_dgm_chi2map.py
@@ -17,6 +17,41 @@ if TYPE_CHECKING:
     from numpy.typing import NDArray
 
 
+def get_profile_of_chi2(
+    parameter_grid: NDArray,
+    profile_grid: NDArray,
+    chi2_map: NDArray,
+    best_fit_value: float,
+    best_fit_fun: float,
+) -> tuple[NDArray, NDArray]:
+    """Make a profile of the chi-squared map using thee minimum value. Works
+    with 2-dimensional maps.
+
+    Parameters
+    ----------
+    parameter_grid : NDArray
+        Array of grid to look for best fit value of parameter.
+    profile_grid : NDArray
+        Array of grid to create profile grid.
+    chi2_map : NDArray
+        Map of chi-squared values.
+    best_fit_value : float
+        Value of parameter in best fit point.
+    best_fit_fun : float
+        Value of the chi-squared in best fit point.
+
+    Returns
+    -------
+    tuple[NDArray, NDArray]
+        Array of profile grid values and array of chi-squared values.
+    """
+    abs_difference = np.abs(parameter_grid - best_fit_value)
+    closest_value = abs_difference.min()
+    mask = abs_difference == closest_value
+    chi2_profile = chi2_map[mask] - best_fit_fun
+    return profile_grid[mask], chi2_profile
+
+
 def cartesian_product(
     grid_opts: list[tuple[str, float, float, int]],
 ) -> tuple[list[str], list[NDArray], NDArray]:
@@ -52,10 +87,10 @@ def main(args: Namespace) -> None:
     parameters_constrained = storage["parameters.constrained"]
     statistic = storage["outputs.statistic"]
 
-    stat_chi2 = statistic[f"{args.chi2}"]
+    stat_chi2 = statistic[f"{args.statistic}"]
     minimization_parameters: dict[str, Parameter] = {}
     update_dict_parameters(minimization_parameters, args.free_parameters, parameters_free)
-    if "covmat" not in args.chi2:
+    if "covmat" not in args.statistic:
         update_dict_parameters(
             minimization_parameters,
             args.constrained_parameters,
@@ -88,18 +123,39 @@ def main(args: Namespace) -> None:
         chi2_map[idx] = fit["fun"]
 
     chi2_map_1d: dict[str, NDArray | Any] = dict.fromkeys(grid_parameters)
-    for parameter, grid_1d in zip(grid_parameters, grids):
-        model.set_parameters(bf_x_dict)
-        model.next_sample(mc_parameters=False, mc_statistics=False)
-        chi2_map_1d[parameter] = np.zeros(grid_1d.shape[0])
-        minimization_parameters_1d = minimization_parameters.copy()
-        minimization_parameters_1d.pop(parameter)
-        minimizer_scan_1d = IMinuitMinimizer(stat_chi2, parameters=minimization_parameters_1d)
-        for idx, grid_value in enumerate(grid_1d):
-            model.set_parameters({parameter: grid_value})
-            fit = minimizer_scan_1d.fit()
-            minimizer_scan_1d.push_initial_values()
-            chi2_map_1d[parameter][idx] = fit["fun"]
+    if args.scan_1d:
+        for parameter, grid_1d in zip(grid_parameters, grids):
+            model.set_parameters(bf_x_dict)
+            model.next_sample(mc_parameters=False, mc_statistics=False)
+            chi2_map_1d[parameter] = np.zeros_like(grid_1d)
+            minimization_parameters_1d = minimization_parameters.copy()
+            minimization_parameters_1d.pop(parameter)
+            minimizer_scan_1d = IMinuitMinimizer(stat_chi2, parameters=minimization_parameters_1d)
+            for idx, grid_value in enumerate(grid_1d):
+                model.set_parameters({parameter: grid_value})
+                fit = minimizer_scan_1d.fit()
+                minimizer_scan_1d.push_initial_values()
+                chi2_map_1d[parameter][idx] = fit["fun"]
+    else:
+        parameter_x, parameter_y = grid_parameters
+        best_fit_x = global_fit["xdict"][parameter_x]
+        best_fit_y = global_fit["xdict"][parameter_y]
+        fun = global_fit["fun"]
+        grid_x, chi2_map_1d[parameter_x] = get_profile_of_chi2(
+            xy_grid[:, 0],
+            xy_grid[:, 1],
+            chi2_map,
+            best_fit_x,
+            fun,
+        )
+
+        grid_y, chi2_map_1d[parameter_y] = get_profile_of_chi2(
+            xy_grid[:, 1],
+            xy_grid[:, 0],
+            chi2_map,
+            best_fit_y,
+            fun,
+        )
 
     if args.output:
         parameter_x, parameter_y = grid_parameters
@@ -107,8 +163,8 @@ def main(args: Namespace) -> None:
             args.output,
             **{
                 "chi2map2d": np.stack((*xy_grid.T, chi2_map), axis=1),
-                "chi2map1d_x": chi2_map_1d["survival_probability.SinSq2Theta13"],
-                "chi2map1d_y": chi2_map_1d["survival_probability.DeltaMSq32"],
+                "chi2map1d_x": np.stack((grids[0], chi2_map_1d[parameter_x]), axis=1),
+                "chi2map1d_y": np.stack((grids[1], chi2_map_1d[parameter_y]), axis=1),
                 "best_fit_values": np.array(
                     [
                         (
@@ -151,7 +207,12 @@ if __name__ == "__main__":
         help="linspace of parameter",
     )
     parser.add_argument(
-        "--chi2",
+        "--scan-1d",
+        action="store_true",
+        help="provide 1d profiling with minos algorithm",
+    )
+    parser.add_argument(
+        "--statistic",
         default="full.pull.chi2cnp",
         choices=[
             "stat.chi2p_iterative",
